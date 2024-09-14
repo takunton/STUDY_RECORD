6ee59bb94cb6055b7b016960999e17046a3fc9cb
Higashiyamazaki_Takuto <higashiyamazaki.takuto@starbridge-iwate.com>
2024-09-14 17:17:09 +0900

    #2 ローカルストレージからセッションストレージにログイン情報を保持するよう変更

--- study-record-frontend-next-supabase/src/_hooks/useLoginInfo.ts
+++ study-record-frontend-next-supabase/src/_hooks/useLoginInfo.ts
@@ -1,20 +1,35 @@
 import { LoginInfo } from "../_types/LoginInfo";
 
-// TODO: ローカルストレージからセッションストレージまたはcookieに変更
 export const setLoginInfo = (value: LoginInfo) => {
-  localStorage.setItem("id", value.id);
-  localStorage.setItem("email", value.email);
+  console.log(
+    `セッションストレージにユーザー情報を保存[user_id=${sessionStorage.getItem(
+      "id"
+    )}]`
+  );
+  console.log(`user_id:${sessionStorage.getItem("id")}`);
+  sessionStorage.setItem("id", value.id);
+  sessionStorage.setItem("email", value.email);
 };
 
 export const getLoginInfo = (): LoginInfo => {
-  console.log(`user_id:${localStorage.getItem("id")}`);
+  console.log(
+    `セッションストレージからユーザー情報を取得[user_id=${sessionStorage.getItem(
+      "id"
+    )}]`
+  );
+  console.log(`user_id:${sessionStorage.getItem("id")}`);
   return {
-    id: localStorage.getItem("id") ?? "",
-    email: localStorage.getItem("email") ?? "",
+    id: sessionStorage.getItem("id") ?? "",
+    email: sessionStorage.getItem("email") ?? "",
   };
 };
 
 export const removeLoginInfo = () => {
+  console.log(
+    `セッションストレージからユーザー情報を削除[user_id=${sessionStorage.getItem(
+      "id"
+    )}]`
+  );
   localStorage.removeItem("id");
   localStorage.removeItem("email");
 };
